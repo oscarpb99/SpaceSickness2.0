5e02af58ec21977f9f7092d5ae806ec16650d951
oscarpb99 <oscarped@ucm.es>
2018-03-07 10:13:13 +0100

    Repositorio Arreglado

---
+++ Assets/Scripts/PruebaTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaTrigger : MonoBehaviour {
+    public bool muestra; 
+	// Use this for initialization
+	void Start () {
+        muestra = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void muestraOn()
+    {
+        muestra = true;
+    }
+
+    public void muestraOff()
+    {
+        muestra = false;
+    }
+}
