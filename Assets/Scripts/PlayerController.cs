e7be919fdff605127a74b01e2b5f92949392a66e
CarlosMGM <gonzalem@ucm.es>
2018-03-12 20:22:46 +0100

    corregir error al hacer merge en player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,10 @@ public class PlayerController : MonoBehaviour {
 
 
 	void Awake () {
-		rb = GetComponentInChildren<Rigidbody2D> ();
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
-		if(controlmovimiento)
         GameManager.instance.SetCurrentGravity(salaactual.GetComponent<GuardaGravedad>());
 
         if (controlmovimiento)
@@ -24,15 +22,11 @@ public class PlayerController : MonoBehaviour {
 		if (Mathf.Abs (rb.velocity.x) <= 0.3f && Mathf.Abs (rb.velocity.y) <= 0.3f)
 			rb.velocity = new Vector2 (0, 0);
 	}
-
 		
 	void Movimiento () {
-		salaactual = this.GetComponentInChildren<CambioGravedad> ().sala;
-		if (GameManager.instance.GetDirection() == DireccionGravedad.Derecha)
 		salaactual = this.GetComponent<CambioGravedad> ().sala;
 		if (salaactual.GetComponent<GuardaGravedad> ().GetDireccion() == DireccionGravedad.Derecha)
 			transform.Translate (new Vector3 (0, Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0));
-		else if (GameManager.instance.GetDirection() == DireccionGravedad.Izquierda)
 		else if (salaactual.GetComponent<GuardaGravedad>().GetDireccion() == DireccionGravedad.Izquierda)
 			transform.Translate (new Vector3 (0, Input.GetAxis ("Horizontal") * -speed * Time.deltaTime, 0));
 		else transform.Translate(new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime,0, 0));
