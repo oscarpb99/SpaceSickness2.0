11ce82b7ff045a456932f5e632445c38b92f5eac
AdrianFP95 <36952552+AdrianFP95@users.noreply.github.com>
2018-03-20 10:58:05 +0100

    Prueba gravedad 0
    Creado un script que hace un leve impulso, al pulsar la tecla espacio

---
+++ Assets/Scripts/Gravedad 0/Individual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Individual : MonoBehaviour {
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		rb.gravityScale = 0f;
+		if (Input.GetKey (KeyCode.Space)) {
+			rb.AddForce(transform.up * 5);
+		}
+
+	}
+
+}
