4e94b8533e2119ce98f8256f7d9785f74157d8e7
oscarpb99 <36916863+oscarpb99@users.noreply.github.com>
2018-04-11 10:23:00 +0200

    inicio del menu pausa

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour {
+	bool pause=false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("p") && pause == false) 
+		{
+			pause = true;
+			Time.timeScale = 0;
+		}
+		else if (Input.GetKeyDown ("p") && pause == true) 
+		{
+			pause = false;
+			Time.timeScale = 1;
+		}
+		
+	}
+}
