793cf25035317b63b3e8311c512109f2f65cc8b5
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-04-16 15:43:33 +0200

    Zoom de las cámaras configurable desde el editor

--- Assets/Scripts/Camara3.cs
+++ Assets/Scripts/Camara3.cs
@@ -8,6 +8,8 @@ public class Camara3 : MonoBehaviour {
 	void Update(){
 		camera = GameManager.instance.GetCamerapos ();
 		gameObject.transform.position = new Vector3 (camera.transform.position.x, camera.transform.position.y, -10);
+		gameObject.GetComponent<Camera> ().orthographicSize = 5*camera.GetComponent<ConfiguracionCamara> ().zoom;
+	
 	}
 
 
---
+++ Assets/Scripts/ConfiguracionCamara.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfiguracionCamara : MonoBehaviour {
+	public float zoom=1;
+
+}
