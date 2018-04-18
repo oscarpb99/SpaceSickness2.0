7aeaad2983712dc70af5289455f11fed805b62a0
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-04-18 10:16:07 +0200

    Arreglados objetos en gravedad 0

--- Assets/Scripts/SleepObjetos.cs
+++ Assets/Scripts/SleepObjetos.cs
@@ -6,10 +6,19 @@ using UnityEngine;
 public class SleepObjetos : MonoBehaviour {
 	GameObject sala=null;
 	public GameObject posinicial;
+	Rigidbody2D rb;
+
+	void Start(){
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (sala != null) {
+			if (sala.GetComponent<GuardaGravedad> ().GetDireccion () == DireccionGravedad.Gravedad0) {
+				rb.gravityScale = 0;
+			} else
+				rb.gravityScale = 1;
 			if (sala==GameManager.instance.salaactual)//si la sala donde está el objeto es la misma que la sala en la que está el jugador 
 				GetComponent<Rigidbody2D> ().simulated = true;//descongela el objeto
 			else
