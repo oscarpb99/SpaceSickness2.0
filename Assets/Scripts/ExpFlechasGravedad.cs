6593051c259816fe04e9f5b04dbc776c51c9c4e1
oscarpb99 <36916863+oscarpb99@users.noreply.github.com>
2018-05-04 12:08:11 +0200

    Añadido el texto de como usar la gravedad

---
+++ Assets/Scripts/ExpFlechasGravedad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpFlechasGravedad : MonoBehaviour {
+	public GameObject panel;
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag == "player") {
+			panel.SetActive(true);
+		}
+	
+	}
+	void OnTriggerExit2D(Collider2D col){
+		if (col.tag == "player") {
+			panel.SetActive(false);
+		}
+	}
+}
