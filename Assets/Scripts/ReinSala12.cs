766376e60d2594ec215c50a2af4d902c29ef78b2
oscarpb99 <36916863+oscarpb99@users.noreply.github.com>
2018-05-18 12:28:44 +0200

    Reinicio al salir de la sala de los enemigos planta

--- Assets/Scripts/ReinSala12.cs
+++ Assets/Scripts/ReinSala12.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReinSala12 : MonoBehaviour {
-	public GameObject sala12;
-	Transform x;
+	public GameObject enemigoPlanta1, enemigoPlanta2;
+	Vector3 posEnePl1,posEnePl2;
+	Quaternion rotEnePl1,rotEnePl2;
+
+
 	void Start(){
-		x = sala12.transform;
+		posEnePl1 = enemigoPlanta1.transform.position;
+		posEnePl2 = enemigoPlanta2.transform.position;
+		rotEnePl1 = enemigoPlanta1.transform.rotation;
+		rotEnePl2 = enemigoPlanta2.transform.rotation;
 	}
 		
-	void OnTriggerEnter2D(Collider2D col){
+	void OnTriggerExit2D(Collider2D col){
 		if (col.tag == "player") {
-			sala12.transform.position = x.position;
-			sala12.transform.rotation = x.rotation;
+			enemigoPlanta1.transform.position = posEnePl1;
+			enemigoPlanta2.transform.position = posEnePl2;
+			enemigoPlanta1.transform.rotation = rotEnePl1;
+			enemigoPlanta2.transform.rotation = rotEnePl2;
+			
 		}
 	}
 }
