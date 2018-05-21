0233d3c3c8f3101a966ee8801dfb9413e8527b23
e <r>
2018-05-21 11:45:59 +0200

    Sonido del tanque de oxigeno

--- Assets/Scripts/TanqueOxigen.cs
+++ Assets/Scripts/TanqueOxigen.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class TanqueOxigen : MonoBehaviour {
 	public float prAumentooxigen=1;//fraccion del maximo de oxigeno que se devuelve al consumir el tanque (1=maximo, 0=nada)
-
+    public AudioSource aire;
 
 	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.tag == "player") {
 			GameManager.instance.AumentaOxigeno (prAumentooxigen);
+            aire.Play();
+            gameObject.GetComponent<SpriteRenderer>().enabled=false;
+            Invoke("Activar", 1f);
 		}
 	}
-
+    private void Activar()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
 }
